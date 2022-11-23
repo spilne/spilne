123efcae2085a0f59a750f3328e1a2652adc15df
zakolenko <zakolenkoroman@gmail.com>
2022-11-23 03:33:58 +0200

    redis4cats-contrib: refactoring

--- redis4cats-contrib/src/main/scala/dobirne/redis4cats/contrib/script/ScriptArgs.scala
+++ redis4cats-contrib/src/main/scala/dobirne/redis4cats/contrib/script/ScriptArgs.scala
@@ -1,12 +1,15 @@
 package dobirne.redis4cats.contrib.script
 
-case class ScriptArgs[+K, +V](keys: List[K] = Nil, values: List[V] = Nil)
+case class ScriptArgs[K, V](keys: List[K] = Nil, values: List[V] = Nil) {
+  def withKeys(ks: List[K]): ScriptArgs[K, V] = copy(keys = ks)
+  def withValues(vs: List[V]): ScriptArgs[K, V] = copy(values = vs)
+}
 
 object ScriptArgs {
   private val _empty: ScriptArgs[Nothing, Nothing] = ScriptArgs(Nil, Nil)
 
   def apply[K, V](): ScriptArgs[K, V] = empty
   def empty[K, V]: ScriptArgs[K, V] = _empty.asInstanceOf[ScriptArgs[K, V]]
-  def keys[K, V](ks: List[K]): ScriptArgs[K, V] = _empty.copy(keys = ks)
-  def values[K, V](vs: List[V]): ScriptArgs[K, V] = _empty.copy(values = vs)
+  def keys[K, V](ks: List[K]): ScriptArgs[K, V] = empty.withKeys(ks)
+  def values[K, V](vs: List[V]): ScriptArgs[K, V] = empty.withValues(vs)
 }
